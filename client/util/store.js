b55806ef5a1e2320b9200ee2fe134148a7a77bfb
Kousa <jami.kousa@helsinki.fi>
2019-06-06 16:27:57 +0300

    do not disable redux devtools in production

--- client/util/store.js
+++ client/util/store.js
@@ -1,12 +1,12 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 
-import { inProduction } from 'Utilities/common'
 import { handleRequest } from 'Utilities/apiConnection'
 import combinedReducers from 'Utilities/redux'
 
 // eslint-disable-next-line
-const composeEnhancers = (!inProduction && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
 const store = createStore(
   combinedReducers,
   composeEnhancers(applyMiddleware(thunk, handleRequest)),
