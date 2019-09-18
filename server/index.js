a951aad69864b3221c3b3008034222b676dfc91c
Jami Kousa <jami.kousa@helsinki.fi>
2019-09-18 14:57:31 +0300

    shibboleth headers

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const routes = require('@util/routes')
 const errorMiddleware = require('@middleware/errorMiddleware')
+const shibbolethCharsetMiddleware = require('@middleware/shibbolethCharsetMiddleware')
 
 const app = express()
 
 app.use(express.json())
+app.use(shibbolethCharsetMiddleware)
 app.use(routes)
 
 app.use(errorMiddleware)
