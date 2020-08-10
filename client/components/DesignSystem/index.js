fae31c82879aaf01490fae3c9250341567f19e14
Jami Kousa <jami.kousa@helsinki.fi>
2020-08-10 15:40:28 +0300

    refactor dev mode code

--- client/components/DesignSystem/index.js
+++ client/components/DesignSystem/index.js
@@ -3,11 +3,7 @@ import { Button } from '@material-ui/core'
 import styles from 'Assets/custom.module.scss'
 
 const DesignSystem = () => {
-  return (
-    <Button className={styles.toskabutton}>
-      Styled with css modules
-    </Button>
-  )
+  return <Button className={styles.toskabutton}>Styled with css modules</Button>
 }
 
 export default DesignSystem
